ed8bcfc3f505d7d133341e58a6db2e88917f1ac6
Sarayut6166-stack <Sarayut6166@icloud.com>
2025-11-26 17:51:27 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAaBGJLth3bXoWBFv3qNL4zi0v_rsXOZwc",
+  authDomain: "sos-rescue-system.firebaseapp.com",
+  projectId: "sos-rescue-system",
+  storageBucket: "sos-rescue-system.firebasestorage.app",
+  messagingSenderId: "1097807907954",
+  appId: "1:1097807907954:web:be172eedc7def9bda2c6eb",
+  measurementId: "G-SZZ9R4VBJS"
+};
